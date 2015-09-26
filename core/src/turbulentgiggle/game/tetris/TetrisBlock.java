bded77e6c470e83690f98c7bca6ece7b4e358dfc
Vincent Lee <vincent.lee@outlook.com>
2015-09-26 16:54:04 -0500

    Piece

--- core/src/turbulentgiggle/game/tetris/TetrisBlock.java
+++ core/src/turbulentgiggle/game/tetris/TetrisBlock.java
@@ -1,5 +1,8 @@
 package turbulentgiggle.game.tetris;
 
+import com.badlogic.gdx.math.Vector2;
+import com.google.common.collect.ImmutableList;
+
 /**
  * Created by Quang on 9/26/2015.
  */
@@ -11,4 +14,10 @@ public enum TetrisBlock {
     L,
     J,
     T;
+
+    final ImmutableList<Vector2> defaultState;
+
+    TetrisBlock(Vector2... positions) {
+        defaultState = ImmutableList.copyOf(positions);
+    }
 }
