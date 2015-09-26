9b93dd072807fb02970476b6ca5c4fb3710aceb8
Vincent Lee <vincent.lee@outlook.com>
2015-09-26 16:47:16 -0500

    No magic numbers!

--- core/src/turbulentgiggle/game/tetris/TetrisBlock.java
+++ core/src/turbulentgiggle/game/tetris/TetrisBlock.java
@@ -3,14 +3,12 @@ package turbulentgiggle.game.tetris;
 /**
  * Created by Quang on 9/26/2015.
  */
-public class TetrisBlock {
-
-    public static int O = 0;
-    public static int I = 1;
-    public static int S = 2;
-    public static int Z = 3;
-    public static int L = 4;
-    public static int J = 5;
-    public static int T = 6;
-
+public enum TetrisBlock {
+    O,
+    I,
+    S,
+    Z,
+    L,
+    J,
+    T;
 }
